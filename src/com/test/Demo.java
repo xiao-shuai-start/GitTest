ee3766940ada59d3b0887982e1994e835771e7fa
xiaoshuai <xiaoshuai1576@163.com>
2020-02-10 19:15:03 +0800

    冲突练习

--- src/com/test/Demo.java
+++ src/com/test/Demo.java
@@ -3,7 +3,8 @@ package com.test;
 public class Demo {
 	public static void main(String[] args) {
 		
-		System.out.println("..........");
+		
 		System.out.println("1111111111111");
+		System.out.println("22222222");
 	}
 }
